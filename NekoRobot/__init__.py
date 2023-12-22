cbdd87c1043fd912e3e96dd588b7b9cbd3b582af
Swami <146053559+VMTY1@users.noreply.github.com>
2023-12-23 00:14:49 +0530

    Update __init__.py

--- NekoRobot/__init__.py
+++ NekoRobot/__init__.py
@@ -341,15 +341,15 @@ DEMONS = list(DEMONS)
 TIGERS = list(TIGERS)
 
 # Load at end to ensure all prev variables have been set
-from NekoRobot.modules.helper_funcs.handlers import (
-    CustomCommandHandler,
-    CustomMessageHandler,
-    CustomRegexHandler,
+#from NekoRobot.modules.helper_funcs.handlers import (
+ #   CustomCommandHandler,
+ #   CustomMessageHandler,
+ #   CustomRegexHandler,
 )
 
 # make sure the regex handler can take extra kwargs
-tg.RegexHandler = CustomRegexHandler
-tg.CommandHandler = CustomCommandHandler
-tg.MessageHandler = CustomMessageHandler
+#tg.RegexHandler = CustomRegexHandler
+#tg.CommandHandler = CustomCommandHandler
+#tg.MessageHandler = CustomMessageHandler
 
-LOGGER.info("[NEKOROBOT IS READY]")
+#LOGGER.info("[NEKOROBOT IS READY]")
